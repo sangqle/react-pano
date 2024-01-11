a4781da51806b04d45100093340b2bb173561894
sanglq3 <sanglq3@vng.com.vn>
2024-01-11 14:02:01 +0700

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
-import { resolve } from "node:path";
+import { resolve } from 'node:path'
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import EsLint from "vite-plugin-linter";
-import tsConfigPaths from "vite-tsconfig-paths";
-const { EsLinter, linterPlugin } = EsLint;
-import * as packageJson from "./package.json";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import EsLint from 'vite-plugin-linter'
+import tsConfigPaths from 'vite-tsconfig-paths'
+const { EsLinter, linterPlugin } = EsLint
+import * as packageJson from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [
     react(),
     tsConfigPaths(),
     linterPlugin({
-      include: ["./src}/**/*.{ts,tsx}"],
+      include: ['./src}/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
     }),
     dts({
-      include: ["src/component/"],
+      include: ['src/component/'],
     }),
   ],
   build: {
     lib: {
-      entry: resolve("src", "component/index.ts"),
-      name: "ReactViteLibrary",
-      formats: ["es", "umd"],
-      fileName: (format) => `react-vite-library.${format}.js`,
+      entry: resolve('src', 'component/index.ts'),
+      name: 'ReactPanorama',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-pano.${format}.js`,
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
-}));
+}))
